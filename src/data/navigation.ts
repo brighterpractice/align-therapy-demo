43b2c7d3a48718fbeb68ad69b9fa54be38728792
brighterpractice <314649874+brighterpractice@users.noreply.github.com>
2026-08-28 18:31:15 -0600

    Simplify Align mid-tier navigation

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -15,27 +15,12 @@ export const navigation = [
       { label: 'Individual Therapy', href: '/services/individual-therapy/' },
       { label: 'Trauma & Difficult Experiences', href: '/services/trauma-ptsd/' },
       { label: 'Anxiety & Stress', href: '/services/anxiety-stress/' },
-      { label: 'Grief & Loss', href: '/services/grief-loss/' },
       {
         label: 'Depression & Emotional Disconnection',
         href: '/services/depression-emotional-disconnection/',
       },
-      {
-        label: 'Chronic Pain & Illness',
-        href: '/services/chronic-pain-chronic-illness/',
-      },
-      {
-        label: 'Life Transitions',
-        href: '/services/life-transitions/',
-      },
-      {
-        label: 'Relationship Concerns',
-        href: '/services/relationship-concerns/',
-      },
-      {
-        label: 'Self-Esteem & Personal Growth',
-        href: '/services/self-esteem-personal-growth/',
-      },
+      { label: 'Life Transitions', href: '/services/life-transitions/' },
+      { label: 'Relationship Concerns', href: '/services/relationship-concerns/' },
     ],
   },
   {
@@ -50,14 +35,6 @@ export const navigation = [
       {
         label: 'Somatic Experiencing',
         href: '/approaches/somatic-experiencing/',
-      },
-      {
-        label: 'Emotionally Focused Therapy',
-        href: '/approaches/emotionally-focused-therapy/',
-      },
-      {
-        label: 'Mindfulness-Based Therapy',
-        href: '/approaches/mindfulness-based-therapy/',
       },
     ],
   },
